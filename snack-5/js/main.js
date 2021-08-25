c9376b39170d087e39a6eda22d72b6ccc0dcc96d
leonardobarone <leonardobarone26@gmail.com>
2021-08-25 15:32:37 +0200

    js fatto

--- snack-5/js/main.js
+++ snack-5/js/main.js
@@ -1,3 +1,15 @@
-alert("Ciao!");
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
 
-// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’arra
+var arr = [];
+
+for (var i = 0; i <= 5; i++ ) {
+    
+    var n = prompt("Inserisci un numero!");
+
+    if ( n % 2 == 1) {
+        arr.push(n);
+    }
+    
+}
+
+console.log(arr);
